92f1ad3916234d8eb943b7d776399638280fa38e
Mert Bağ <bagmert1996@gmail.com>
2024-11-28 21:47:33 +0300

    Layout Update

--- resources/views/auth/_layout.blade.php
+++ resources/views/auth/_layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Sales.io - @yield('title')</title>
 </head>
@@ -12,8 +13,8 @@
 <body class="py-24 flex justify-center items-center">
     <form action="@yield('action')" method="POST" class="w-1/5" autocomplete="off">
         @csrf
-        <div class="mb-4">
-            <h3 class="font-bold text-3xl text-center">Sales.io</h3>
+        <div class="mb-4 flex justify-center">
+            <img src="{{ asset('logo.JPG') }}" alt="" width="144px">
         </div>
         @yield('content')
     </form>
